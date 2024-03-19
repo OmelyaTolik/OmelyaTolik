933250c6fcc8092ae7e6ae0d500930b64462c20c
OmelyaTolik <omelchenkoanatolii4@gmail.com>
2024-03-19 09:41:48 +0200

    Removed tests

---
+++ helper_lty2_185300_gd7e.tsx
@@ -0,0 +1,10 @@
+banana
+
+const removeDuplicates = array => Array.from(new Set(array));
+
+1 - 89,78,73,35,3,56,23,89,62,52,81,32,47,19,39,25,51,69,42,3,16,56,19
+const calculateFactorial = n => n === 0 ? 1 : n * calculateFactorial(n - 1);
+orange
+
+const capitalizeString = str => str.toUpperCase();
+const snakeCaseToCamelCase = str => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
